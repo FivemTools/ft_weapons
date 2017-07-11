a5f6b9027e935b68876c8b913de2696f8be66ee0
CanadienFr <provenchja2@msn.com>
2017-07-11 01:46:55 -0400

    Update 0.1.2

--- sv_weapons.lua
+++ sv_weapons.lua
@@ -7,7 +7,6 @@ AddEventHandler("ft_weapons:SvloadWeapons", function()
   TriggerEvent("ft_gamemode:SvGetPlayerData", source, "steamId", function (steamId)
   
     MySQL.Async.fetchAll("SELECT * FROM weapons WHERE steamId = @name", {['@name'] = steamId}, function(weapons)
-      TriggerEvent("ft_debugBase:SvDebug", { data = weapons })
       TriggerClientEvent('ft_weapons:ClgiveWeapons', source, weapons)
     end)
     
